349bcb58adb0591b3d8ffc63b93f0c654893257f
Reginald1632 <reginald1632@icloud.com>
2026-06-07 13:47:38 +0000

    Add files via upload

--- Hello.java
+++ Hello.java
@@ -2,7 +2,31 @@
    
    public static void main(String[] agrs){
 
-     System.out.print("Hello world");
+     String fullname   = "Gyamena Reginald Afrifa";
+     int     age       = 22;
+     char    gender    = 'M';
+     double  height    = 1.65;
+     float   weight    = 58.5f;
+     boolean isStudent = true;
+     long phoneNumber  = 233201371249L;
+     String grade      = "Level 200";
+     String major      = "Computer Science";
+     double gpa        = 3.78;
+
+     
+     System.out.println("Full Name    : " + fullname);
+     System.out.println("Age          : " + age + " years");
+     System.out.println("Gender       : " + gender);
+     System.out.println("Height       : " + height + " m");
+     System.out.println("Weight       : " + weight + " kg");
+     System.out.println("Students     : " + isStudent);
+     System.out.println("Phone Number : " + phoneNumber);
+     System.out.println("Grade/Level  : " + grade);
+     System.out.println("Major        : " + major);
+     System.out.println("GPA          : " + gpa);
+   
+
+
   }
 
 
