bcedba9238e50a9ce304b382c0ab66a0f6a84347
Reginald1632 <reginald1632@icloud.com>
2026-06-09 13:34:21 +0000

    Add files via upload

---
+++ Self.java
@@ -0,0 +1,18 @@
+public class Self {
+    public static void main(String[] args) {
+        String name = " Reginald Afrifa Gyamena";
+        int age =  22;
+        char sex = 'M';
+        long phone = 233566654376L;
+        boolean student = true;
+        float height = 175.344F;
+
+
+        System.out.println("My names is " +name);
+        System.out.println("My age is " +age);
+        System.out.println("Male or Female " +sex);
+        System.out.println("My phone number is " +phone);
+        System.out.println("I'm a studnet " +student);
+        System.out.println("My height is " +height);
+    }
+}
